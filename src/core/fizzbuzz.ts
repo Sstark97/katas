c339df6fbf92b0c57348d3b7dff7099a88d041cc
Sstark97 <aitorscinfo@gmail.com>
2023-03-16 13:33:16 +0000

    Test(Green): Check_that_fizzbuzz_of_15_is_fizzbuzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,5 +1,7 @@
 const whoFizzBuzzElementIs = (num: number) => {
-    if (num % 3 === 0) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "fizzbuzz"
+    } else if (num % 3 === 0) {
         return "fizz"
     } else if (num % 5 === 0) {
         return "buzz"
