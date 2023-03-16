2269d0710d5268261a3bae3b4f921d1083e05c32
Sstark97 <aitorscinfo@gmail.com>
2023-03-16 12:55:28 +0000

    Test(Refactor): Check_that_number_3_returns_fizz

--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -3,7 +3,8 @@ import { fizzbuzz } from "../core/fizzbuzz";
 describe('Fizzbuzz Test', () => {
     it("check that number 3 returns fizz", () => {
         const fizzbuzzResult = fizzbuzz()
+        const fizz = fizzbuzzResult.find((num, pos) => num === "fizz" && (pos + 1) % 3 === 0)
 
-        expect(fizzbuzzResult[2]).toBe("fizz")
+        expect(fizz).toBe("fizz")
     })
 });
