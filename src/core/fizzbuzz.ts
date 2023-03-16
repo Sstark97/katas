91d4c245c4d1dc1a6bd1d1ba9bf3d577aff3cbf6
Sstark97 <aitorscinfo@gmail.com>
2023-03-16 13:06:11 +0000

    Test(Green): Check_that_number_5_returns_buzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,8 +1,10 @@
 export const fizzbuzz = () => {
-    const arr = [1, 2, 3];
+    const arr = [1, 2, 3, 4, 5];
     const fizzBuzzResult: (string | number)[] = arr.map(num => {
         if(num % 3 === 0) {
             return "fizz"
+        } else if(num % 5 === 0) {
+            return "buzz"
         }
         return num
     })
