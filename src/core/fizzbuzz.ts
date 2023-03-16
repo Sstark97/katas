abedfb869951b29df7ae9e887bcd6e2c1a1ec349
Sstark97 <aitorscinfo@gmail.com>
2023-03-16 13:16:20 +0000

    Test(Refactor): Check_that_number_5_returns_buzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,13 +1,14 @@
+const whoFizzBuzzElementIs = (num: number) => {
+    if (num % 3 === 0) {
+        return "fizz"
+    } else if (num % 5 === 0) {
+        return "buzz"
+    }
+    return num
+}
+
 export const fizzbuzz = () => {
     const arr = [1, 2, 3, 4, 5];
-    const fizzBuzzResult: (string | number)[] = arr.map(num => {
-        if(num % 3 === 0) {
-            return "fizz"
-        } else if(num % 5 === 0) {
-            return "buzz"
-        }
-        return num
-    })
 
-    return fizzBuzzResult
+    return arr.map(num => whoFizzBuzzElementIs(num))
 };
