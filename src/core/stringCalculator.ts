239354184431adb19963d259b2e956b0fcb52c22
Sstark97 <aitorscinfo@gmail.com>
2023-03-17 08:53:01 +0000

    Test(green): should_return_the_sum_of_numbers_contains_in_string_separated_by_commas

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,3 +1,10 @@
 export const add = (theOperationInThis: string) => {
-    return theOperationInThis === "" ? "0" : theOperationInThis
+    const theOperationInThisToIterate = theOperationInThis.split((","))
+    let sum = 0
+
+    for (let i = 0; i < theOperationInThisToIterate.length; i++) {
+        sum += parseInt(theOperationInThisToIterate[i])
+    }
+
+    return theOperationInThis === "" ? "0" : sum.toString()
 }
