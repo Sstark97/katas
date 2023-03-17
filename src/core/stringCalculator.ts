d607f42834463b86f462f9eef40f92db376d38fa
Sstark97 <aitorscinfo@gmail.com>
2023-03-17 10:24:31 +0000

    Test(refactor): should_return_error_message_if_a_number_is_missing_in_last_position

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,4 +1,4 @@
-function checkIfTwoSeparatorsAreTogetherIn(theOperation: string) {
+const checkIfTwoSeparatorsAreTogetherIn = (theOperation: string) => {
     theOperation.split('').forEach((char, index) => {
         const currentElement = char.match(/[,\n]/)
         const nextElement = theOperation[index+1]?.match(/[,\n]/);
@@ -9,12 +9,16 @@ function checkIfTwoSeparatorsAreTogetherIn(theOperation: string) {
     });
 }
 
-export const add = (theOperation: string) => {
+const checkIfSeparatorIsInTheLastPositionOf = (theOperation: string) => {
     if (theOperation.at(-1)?.match(/[,\n]/)) {
         throw new Error("Number expected but EOF found.");
     }
+}
 
+export const add = (theOperation: string) => {
+    checkIfSeparatorIsInTheLastPositionOf(theOperation);
     checkIfTwoSeparatorsAreTogetherIn(theOperation);
+
     const theOperationInThisToIterate = theOperation.split(/[,\n]/)
     const sum = theOperationInThisToIterate.reduce((previousNumber, currentNumber) => previousNumber + parseFloat(currentNumber), 0)
 
