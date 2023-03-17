f253b61ca6ec595f0d19642b3791b4a0e38cb2b4
Sstark97 <aitorscinfo@gmail.com>
2023-03-17 08:39:01 +0000

    Test(red): should_return_the_same_number_if_the_string_only_contains_one_number

--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -3,5 +3,9 @@ import {add} from "../core/stringCalculator";
 describe("String Calculator test", () => {
     it("should return 0 for empty string", () => {
         expect(add("")).toBe("0")
-    });
+    })
+
+    it("should return the same number if the string only contains one number", () => {
+        expect(add("1")).toBe("1")
+    })
 })
