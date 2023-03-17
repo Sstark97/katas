a20031722e0bc1c58c5dcfa09b9c58863bcccd27
Sstark97 <aitorscinfo@gmail.com>
2023-03-17 09:18:25 +0000

    Test(green): should_return_the_sum_of_numbers_contains_in_string_separated_by_commas_and_new_lines

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,5 +1,5 @@
 export const add = (theOperationInThis: string) => {
-    const theOperationInThisToIterate = theOperationInThis.split((","))
+    const theOperationInThisToIterate = theOperationInThis.split(/[,\n]/)
     const sum = theOperationInThisToIterate.reduce((previousNumber, currentNumber) => previousNumber + parseFloat(currentNumber), 0)
 
     return theOperationInThis === "" ? "0" : sum.toString()
--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -17,7 +17,7 @@ describe("String Calculator test", () => {
         expect(add("1, 2, 3")).toBe("6")
     })
 
-    it("should return the sum of numbers contains in string separated by commas", () => {
+    it("should return the sum of numbers contains in string separated by commas and newlines", () => {
         expect(add("1\n2, 3")).toBe("6")
     })
 })
