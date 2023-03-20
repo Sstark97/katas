d7db8e9e152a3d51c673cc2a1761f4cbe2739bb4
Sstark97 <aitorscinfo@gmail.com>
2023-03-20 13:37:01 +0000

    Test(refactor): throw_an_error_if_columnNumber_it´s_negative

--- src/core/wrap.ts
+++ src/core/wrap.ts
@@ -1,7 +1,11 @@
-export const wrap = (text: string, columnNumber: number) => {
-    if(columnNumber < 0) {
+const checkIfIsNegative = (columnNumber: number) => {
+    if (columnNumber < 0) {
         throw new Error(`Error: columnNumber must be a positive number and it is: ${columnNumber}`)
     }
+}
+
+export const wrap = (text: string, columnNumber: number) => {
+    checkIfIsNegative(columnNumber);
 
     return text;
 }
