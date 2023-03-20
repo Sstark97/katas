64101433e35664253792ce66682cc0431426e03f
Sstark97 <aitorscinfo@gmail.com>
2023-03-20 13:35:44 +0000

    Test(green): throw_an_error_if_columnNumber_it´s_negative

--- src/core/wrap.ts
+++ src/core/wrap.ts
@@ -1,3 +1,7 @@
 export const wrap = (text: string, columnNumber: number) => {
+    if(columnNumber < 0) {
+        throw new Error(`Error: columnNumber must be a positive number and it is: ${columnNumber}`)
+    }
+
     return text;
 }
