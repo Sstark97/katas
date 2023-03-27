0e6af438e874da91af0fc1a812412665b459ad78
Sstark97 <aitorscinfo@gmail.com>
2023-03-27 11:18:12 +0100

    Test(refactor): number_five_should_return_V

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -1,8 +1,56 @@
 package org.example;
 
+import java.util.Map;
+
 public class RomanNumerals {
+    static Map<Integer, String> roman = Map.of(
+            1, "I",
+            4, "IV",
+            5, "V",
+            9, "IX",
+            10, "X"
+    );
+    public static void main (String[] args) {
+        Map<Integer, String> roman = Map.of(
+                1, "I",
+                4, "IV",
+                5, "V",
+                9, "IX",
+                10, "X"
+        );
+        int number = 49;
+
+        var test = roman.entrySet().stream()
+                .sorted(Map.Entry.<Integer, String>comparingByKey().reversed()).iterator();
+
+        // Iterate over the map
+        while (test.hasNext()) {
+            var entry = test.next();
+            while(number >= entry.getKey()) {
+                // if number is greater than key, print value
+                System.out.print(entry.getValue());
+                // subtract key from number
+                number -= entry.getKey();
+            }
+        }
+
+    }
     public static String transform(int number) {
-        String numberInRoman = number < 4 ? "I".repeat(number) : "IV";
-        return number > 4 ? "V" : numberInRoman;
+        var romanIterator = RomanNumerals.roman.entrySet().stream()
+                .sorted(Map.Entry.<Integer, String>comparingByKey().reversed()).iterator();
+        String result = "";
+
+        // Iterate over the map
+        while (romanIterator.hasNext()) {
+            var entry = romanIterator.next();
+            while(number >= entry.getKey()) {
+                // if number is greater than key, print value
+                result += entry.getValue();
+                // subtract key from number
+                number -= entry.getKey();
+            }
+        }
+
+        return result;
     }
 }
