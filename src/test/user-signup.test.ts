fb9369036e9e901e1a5d15399d64791a6e746653
Sstark97 <aitorscinfo@gmail.com>
2023-03-28 14:06:14 +0100

    Test(red): throws_an_error_message_if_email_not_have_the_correct_format

--- src/test/user-signup.test.ts
+++ src/test/user-signup.test.ts
@@ -13,4 +13,14 @@ describe("Sign-up", () => {
         expect(spy).toHaveBeenCalled()
         expect(users.length).toBe(0)
     })
+
+    it("throws an error message if email not have the correct format", () => {
+        const fakeDb = new FakeDataBase()
+        const service = new UserService(fakeDb)
+        const spy = jest.spyOn(service, "save")
+        const email = "notAnEmail.com"
+
+        expect(() => service.save(email)).toThrow(`${email} not have the email format`)
+        expect(spy).toHaveBeenCalled()
+    })
 })
