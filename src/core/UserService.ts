42dbcaf0fc351529f078a202e32c3ca413924fca
Sstark97 <aitorscinfo@gmail.com>
2023-03-28 14:16:30 +0100

    Test(green): throws_an_error_message_if_email_not_have_the_correct_format

--- src/core/UserService.ts
+++ src/core/UserService.ts
@@ -7,6 +7,10 @@ export class UserService {
         if (email !== "") {
             this.userRepository.save(email)
         }
+
+        if (email && !email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+            throw new Error(`${email} not have the email format`)
+        }
     }
 
     getUsers() {
