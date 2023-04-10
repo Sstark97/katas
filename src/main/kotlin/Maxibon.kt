a772dbedf233637a5149f3592594938f4235bad3
Sstark97 <aitorscinfo@gmail.com>
2023-04-10 13:08:38 +0100

    Test(refactor): check that there are 12 ice creams when started with 2

--- src/main/kotlin/Maxibon.kt
+++ src/main/kotlin/Maxibon.kt
@@ -3,7 +3,7 @@ class Maxibon(private val slackApi: SlackApi, private var maxibons: Int = 10) {
     private val INCREMENT: Int = 10
 
     init {
-        this.maxibons = if (this.maxibons <= 2) this.maxibons + 10 else this.maxibons
+        this.maxibons = if (this.maxibons <= this.LIMIT) this.maxibons + this.LIMIT else this.maxibons
     }
 
     fun getMaxibons (): Int {
