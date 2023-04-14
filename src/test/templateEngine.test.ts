57e93584bf0ad437724f8431c5a7e70efd5655a5
Sstark97 <aitorscinfo@gmail.com>
2023-04-14 11:59:23 +0100

    Green: empty string are allowed

--- src/test/templateEngine.test.ts
+++ src/test/templateEngine.test.ts
@@ -1,3 +1,7 @@
+function templateEngine(s: string, param2: {}) {
+    return "";
+}
+
 describe("Template Engine", () => {
     it("empty string are allowed", () => {
         expect(templateEngine("", {})).toBe("")
