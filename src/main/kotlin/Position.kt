e4f8c67afb0bb5529332597542cc726159b4ea19
Sstark97 <aitorscinfo@gmail.com>
2023-04-19 12:09:28 +0100

    Test(green): check if position is not negative

--- src/main/kotlin/Position.kt
+++ src/main/kotlin/Position.kt
@@ -1,3 +1,7 @@
-class Position {
-
+class Position(private val x: Int, private val y: Int) {
+    init {
+        if (this.x < 0 || this.y < 0) {
+            throw PositionOutOfBoundsException("Negative position not allowed")
+        }
+    }
 }
---
+++ src/main/kotlin/PositionOutOfBoundsException.kt
@@ -0,0 +1,1 @@
+class PositionOutOfBoundsException (message: String) : Exception(message)
