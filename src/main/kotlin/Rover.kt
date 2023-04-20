3e648a4d7bd4011a3c601081599d9efe26b9656f
Sstark97 <aitorscinfo@gmail.com>
2023-04-20 14:08:31 +0100

    Test(refactor): check if curiosity moves and turns correctly

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -12,11 +12,9 @@ class Rover(private val position: Position, private var direction: DIRECTION) {
                 movement == MOVE.BACKWARD && this.direction == DIRECTION.NORTH -> {
                     this.position.moveVertically(1)
                 }
-                movement == TURN.LEFT -> {
-                    this.direction = DIRECTION.WEST
-                }
-                movement == TURN.RIGHT -> {
-                    this.direction = DIRECTION.EAST
+
+                movement is TURN -> {
+                    this.direction = nextDirectionToFace(movement)
                 }
             }
         }
@@ -28,5 +26,22 @@ class Rover(private val position: Position, private var direction: DIRECTION) {
 
     fun getCurrentDirection(): DIRECTION {
         return this.direction
+    }
+
+    fun nextDirectionToFace(command: TURN): DIRECTION {
+        val cardinalPoints = listOf(DIRECTION.NORTH, DIRECTION.EAST, DIRECTION.SOUTH, DIRECTION.WEST)
+        val currentPoint = cardinalPoints.indexOf(this.direction)
+
+        if(command == TURN.RIGHT){
+            if (currentPoint == 3) {
+                return cardinalPoints.first()
+            }
+            return cardinalPoints[currentPoint + 1]
+        } else {
+            if(currentPoint == 0) {
+                return  cardinalPoints.last()
+            }
+            return cardinalPoints[currentPoint - 1]
+        }
     }
 }
