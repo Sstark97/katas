286ec7dfbb560347fac13735df15ae6bd307badc
Sstark97 <aitorscinfo@gmail.com>
2023-05-05 13:29:47 +0100

    Test(🔴): does not notifies anything if only moves

--- src/test/Mouse.test.ts
+++ src/test/Mouse.test.ts
@@ -15,7 +15,7 @@ describe('Mouse', () => {
         expect(mouseEventListener.handleMouseEvent).toHaveBeenCalledWith(MouseEventType.SingleClick)
     })
 
-    it('does not notify nothing if only release button', () => {
+    it('does not notify anything if only release button', () => {
         const mouse = new Mouse()
         const time = new Date().getTime();
         let mouseEventListener: MouseEventListener = {
@@ -42,5 +42,20 @@ describe('Mouse', () => {
         mouse.move(from,to,time + 10)
 
         expect(mouseEventListener.handleMouseEvent).toHaveBeenCalledWith(MouseEventType.Drag)
+    })
+
+    it('does not notifies anything if only moves', () => {
+        const mouse = new Mouse()
+        const time = new Date().getTime();
+        const from = new MousePointerCoordinates(0,0)
+        const to = new MousePointerCoordinates(10,10)
+        let mouseEventListener: MouseEventListener = {
+            handleMouseEvent: jest.fn()
+        };
+
+        mouse.subscribe(mouseEventListener)
+        mouse.move(from,to,time + 10)
+
+        expect(mouseEventListener.handleMouseEvent).not.toHaveBeenCalled()
     })
 })
