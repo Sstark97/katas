aeba9279fee426698a2c8567701986dd1036f100
Sstark97 <aitorscinfo@gmail.com>
2023-06-27 12:28:55 +0100

    Test(:rotating_light:): check if contains at least eight characters

--- src/test/password_validator.test.ts
+++ src/test/password_validator.test.ts
@@ -4,4 +4,8 @@ describe("Password validator should", () => {
     it("check all security rules", () => {
         expect(passwordValidator("Pa5s_ord")).toBeTruthy()
     })
+
+    it("check if contains at least eight characters", () => {
+        expect(passwordHaveTheMinimumCharacters(password)).toBeTruthy()
+    })
 })
