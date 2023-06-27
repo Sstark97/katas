192812e08dbf1e2e619fc1199a22e4d03638c044
Sstark97 <aitorscinfo@gmail.com>
2023-06-27 12:30:20 +0100

    Test(:white_check_mark:): check if contains at least eight characters

--- src/test/password_validator.test.ts
+++ src/test/password_validator.test.ts
@@ -1,11 +1,15 @@
 import {passwordValidator} from "../core/password_validator";
 
+function passwordHaveTheMinimumCharacters(password: string) {
+    return password.length === 8;
+}
+
 describe("Password validator should", () => {
     it("check all security rules", () => {
         expect(passwordValidator("Pa5s_ord")).toBeTruthy()
     })
 
     it("check if contains at least eight characters", () => {
-        expect(passwordHaveTheMinimumCharacters(password)).toBeTruthy()
+        expect(passwordHaveTheMinimumCharacters("password")).toBeTruthy()
     })
 })
