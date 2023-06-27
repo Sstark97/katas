ab35fa35da47ce23c52270945203ae10659d8f22
Sstark97 <aitorscinfo@gmail.com>
2023-06-27 12:51:50 +0100

    Test(:white_check_mark:): contains at least one underscore

--- src/test/password_validator.test.ts
+++ src/test/password_validator.test.ts
@@ -6,6 +6,10 @@ import {
     passwordValidator
 } from "../core/password_validator";
 
+function passwordContainsNumbers(password: string) {
+    return password.match(/[0-9]+/);
+}
+
 describe("Password validator should", () => {
     it("check all security rules", () => {
         expect(passwordValidator("Pa5s_ord")).toBeTruthy()
