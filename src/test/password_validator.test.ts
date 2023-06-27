53abfd5f543fe602ba2907d9f3f79301aa2578c0
Sstark97 <aitorscinfo@gmail.com>
2023-06-27 12:37:05 +0100

    Test(:white_check_mark:): check if contains at least one lowercase

--- src/test/password_validator.test.ts
+++ src/test/password_validator.test.ts
@@ -1,5 +1,9 @@
 import {passwordHaveTheMinimumCharacters, passwordValidator} from "../core/password_validator";
 
+function passwordContainsLowerCase(password: string) {
+    return password.match(/[a-z]+/);
+}
+
 describe("Password validator should", () => {
     it("check all security rules", () => {
         expect(passwordValidator("Pa5s_ord")).toBeTruthy()
