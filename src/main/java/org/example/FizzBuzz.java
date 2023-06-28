f0e62f967f3f535bf3462903d43cd43ba94c4ee4
Sstark97 <aitorscinfo@gmail.com>
2023-06-28 10:58:58 +0100

    Test(:white_check_mark:): must_be_fizz_for_number_three

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class FizzBuzz {
     public static String execute(int number) {
+        if(number == 3) {
+            return "fizz";
+        }
         return "1";
     }
 }
