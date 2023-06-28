6c9408db16bd26164b1aa33140140fbcfae8c614
Sstark97 <aitorscinfo@gmail.com>
2023-06-28 11:38:18 +0100

    :heavy_plus_sign: Add jqwik for property based testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("net.jqwik:jqwik:1.7.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
