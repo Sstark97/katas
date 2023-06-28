c3bd13228f20a52ab27847e1c1e8707de0b8217e
Sstark97 <aitorscinfo@gmail.com>
2023-06-28 10:51:25 +0100

    Test(:rotating_light:): must_be_fizz_for_number_three

--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -9,4 +9,9 @@ class FizzBuzzTest {
     public void must_be_one_for_number_one () {
         assertEquals("1", FizzBuzz.execute(1));
     }
+
+    @Test
+    public void must_be_fizz_for_number_three () {
+        assertEquals("fizz", FizzBuzz.execute(3));
+    }
 }
