8edf028eea07c3be6808ff7fefc36a183c7747dc
Sstark97 <aitorscinfo@gmail.com>
2023-06-28 11:19:43 +0100

    Test(:white_check_mark:): must_be_zero_for_zero

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class FizzBuzz {
     public static String execute(int number) {
+        if(number == 0) {
+            return "0";
+        }
+
         if (number % 15 == 0) {
             return "fizzbuzz";
         } else if (number % 3 == 0) {
