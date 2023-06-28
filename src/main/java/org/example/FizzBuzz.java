8b08274f1ac8c12f93390827c90cb74d44610353
Sstark97 <aitorscinfo@gmail.com>
2023-06-28 11:07:06 +0100

    Test(:white_check_mark:): must_be_fizz_for_any_number_divisible_by_three

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,12 +2,12 @@ package org.example;
 
 public class FizzBuzz {
     public static String execute(int number) {
-        if (number == 3) {
+        if (number == 15) {
+            return "fizzbuzz";
+        } else if (number % 3 == 0) {
             return "fizz";
         } else if (number == 5) {
             return "buzz";
-        } else if (number == 15) {
-            return "fizzbuzz";
         }
 
         return "1";
