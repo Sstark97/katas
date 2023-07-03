957e88aa70c86cb4888f637c2179ecf931e1e78c
Sstark97 <aitor@leanmind.es>
2023-07-03 08:19:24 +0100

    Test(:rotating_light:): for a word with the first letter, it gives the same word capitalize

--- src/test/camelCaseConverter.test.ts
+++ src/test/camelCaseConverter.test.ts
@@ -16,4 +16,8 @@ describe("Camel Case Converter", () => {
     it("given words with the first letter in upper case separated by dashes, it gives the words joined", () => {
         expect(camelCaseConverter("Foo_Bar-Foo")).toBe("FooBarFoo")
     })
+
+    it("for a word with the first letter, it gives the same word capitalize", () => {
+        expect(camelCaseConverter("foo")).toBe("Foo")
+    })
 })
