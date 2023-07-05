d148eccd67dbb105e0533349cd84c23050bf9d8f
Sstark97 <aitor@leanmind.es>
2023-07-05 11:28:49 +0100

    Test(:recycle:): instructs the recorder to stop recording when the motion sensor does not detect movement

--- src/test/surveillance-controller.test.ts
+++ src/test/surveillance-controller.test.ts
@@ -2,15 +2,14 @@ import {FakeRecorder, FakeSensor, SurveillanceController} from "../core/surveill
 
 describe("Surveillance Controller", () => {
     it("instructs the recorder to stop recording when the motion sensor does not detect movement", () => {
-        let called = false
         const fakeRecorder = new FakeRecorder();
-        fakeRecorder.stopRecording = () => called = true
         const fakeSensor = new FakeSensor();
         const controller = new SurveillanceController(fakeRecorder, fakeSensor);
+        jest.spyOn(fakeRecorder, "stopRecording")
 
         controller.recordMotion()
 
-        expect(called).toBeTruthy()
+        expect(fakeRecorder.stopRecording).toHaveBeenCalled()
     })
 
     it("Instructs the recorder to start recording when the motion sensor detects movement.", () => {
