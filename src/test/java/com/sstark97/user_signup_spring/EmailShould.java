24193c5c59abfa3abb37bd589fca1542f9981536
Sstark97 <aitor@leanmind.es>
2023-08-23 19:33:41 +0100

    Test(:rotating_light:): get_an_error_message_with_a_bad_email_format

---
+++ src/test/java/com/sstark97/user_signup_spring/EmailShould.java
@@ -0,0 +1,16 @@
+package com.sstark97.user_signup_spring;
+
+import com.sstark97.user_signup_spring.domain.model.Email;
+import org.junit.jupiter.api.Test;
+import io.vavr.control.Either;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class EmailShould {
+    @Test
+    public void get_an_error_message_with_a_bad_email_format() {
+        Either<String, Email> email = Email.of("bademail");
+
+        assertThat(email.isLeft()).isTrue();
+        assertThat(email.getLeft()).isEqualTo("The email have a bad format");
+    }
+}
