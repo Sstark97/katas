e2f996075559e2d73921eb4111dc338285659062
Sstark97 <aitor@leanmind.es>
2023-08-23 20:06:50 +0100

    Test(:rotating_light:): get_an_error_message_with_a_password_of_3_characters_or_less

---
+++ src/test/java/com/sstark97/user_signup_spring/PasswordShould.java
@@ -0,0 +1,16 @@
+package com.sstark97.user_signup_spring;
+
+import io.vavr.control.Either;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class PasswordShould {
+    @Test
+    void get_an_error_message_with_a_password_of_3_characters_or_less() {
+        Either<String, Password> email = Password.of("na");
+
+        assertThat(email.isLeft()).isTrue();
+        assertThat(email.getLeft()).isEqualTo("The name must have 3 characters or more");
+    }
+}
