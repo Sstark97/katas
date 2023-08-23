4ebda589b7f6f3f32ed3152b601f08ff0efac8b1
Sstark97 <aitor@leanmind.es>
2023-08-23 20:12:52 +0100

    Test(:white_check_mark:): get_a_with_a_name_with_correct_length

--- src/main/java/com/sstark97/user_signup_spring/domain/model/Name.java
+++ src/main/java/com/sstark97/user_signup_spring/domain/model/Name.java
@@ -10,6 +10,13 @@ public class Name {
     }
 
     public static Either<String, Name> of(String name) {
-        return Either.left("The name must have 3 characters or more");
+        if(name.length() < 3) {
+            return Either.left("The name must have 3 characters or more");
+        }
+        return Either.right(new Name(name));
+    }
+
+    public String getName() {
+        return name;
     }
 }
