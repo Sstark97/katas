6dbbfb84ec95cbd1a5b97b4934916dfae012b458
Sstark97 <aitor@leanmind.es>
2023-08-23 17:51:02 +0100

    Test(:rotating_light:) return_400_when_sign_up_with_a_bad_email_format

--- src/test/java/com/sstark97/user_signup_spring/UserSignUpShould.java
+++ src/test/java/com/sstark97/user_signup_spring/UserSignUpShould.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sstark97.user_signup_spring.domain.UserSignUp;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,5 +38,25 @@ class UserSignUpShould {
                 )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", Matchers.equalTo("name")));
+    }
+
+    @Test
+    void return_400_when_sign_up_with_a_bad_email_format() throws Exception {
+        UserSignUpRepository repository = Mockito.mock(UserSignUpRepository.class);
+        UserSignUpService signUpService = new UserSignUpService(repository);
+        UserSignUp user = new UserSignUp("name", "badFormatEmail", "@Passw0rd");
+        String userToJson = mapper.writeValueAsString(user);
+
+        Mockito.when(repository.save(user)).thenReturn("The email have a bad format");
+
+        MvcResult request = mockMvc.perform(
+                        post("/user/sign_up")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(userToJson)
+                )
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        assertThat(request.getResponse().getContentAsString()).isEqualTo("The email have a bad format");
     }
 }
