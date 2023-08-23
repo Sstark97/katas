41d66caeb42ca544fee0efd10089dce9c544f856
Sstark97 <aitor@leanmind.es>
2023-08-23 20:04:56 +0100

    Test(:white_check_mark:): return_400_when_name_not_have_the_correct_length

--- src/main/java/com/sstark97/user_signup_spring/infrastructure/adapters/UserSignupAdapter.java
+++ src/main/java/com/sstark97/user_signup_spring/infrastructure/adapters/UserSignupAdapter.java
@@ -1,7 +1,6 @@
 package com.sstark97.user_signup_spring.infrastructure.adapters;
 
 import com.sstark97.user_signup_spring.domain.model.ApiError;
-import com.sstark97.user_signup_spring.domain.model.Email;
 import com.sstark97.user_signup_spring.domain.model.UserSignUp;
 import com.sstark97.user_signup_spring.domain.repositories.UserSignUpRepository;
 import com.sstark97.user_signup_spring.infrastructure.dto.UserDto;
@@ -11,13 +10,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserSignupAdapter implements UserSignUpRepository {
-
-    public static final String BAD_FORMAT_ERROR = "The email have a bad format";
     public static final String EMAIL_ALREADY_EXIST_ERROR = "These email already exist";
 
     @Override
     public Either<ApiError, String> save(UserDto userDto) {
         Either<String, UserSignUp> user = UserSignUp.of(userDto);
+        String name = userDto.name();
+        if(name.length() < 3) {
+            return Either.left(new ApiError("The name must have 3 characters or more", HttpStatus.BAD_REQUEST));
+        }
         if(user.isLeft()) {
             return Either.left(new ApiError(user.getLeft(), HttpStatus.BAD_REQUEST));
         }
