7e6a9ff3513c99b352f0b802085b25033cc71187
Sstark97 <aitor@leanmind.es>
2023-08-28 19:54:03 +0100

    Test(:recycle:): PasswordShould get_an_error_message_with_a_password_of_8_characters_or_less

--- src/main/java/com/sstark97/user_signup_spring/domain/model/Password.java
+++ src/main/java/com/sstark97/user_signup_spring/domain/model/Password.java
@@ -3,16 +3,21 @@ package com.sstark97.user_signup_spring.domain.model;
 import io.vavr.control.Either;
 
 public class Password {
-    private String password;
+    private static final int MINIMUM_LENGTH = 8;
+    private final String password;
 
     private Password(String password) {
         this.password = password;
     }
 
     public static Either<String, Password> of(String password) {
-        if(password.length() < 8) {
+        if(password.length() < MINIMUM_LENGTH) {
             return Either.left("The password must have 8 characters or more");
         }
         return Either.right(new Password(password));
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
