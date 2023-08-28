888c4e85d6d6f81e21c4b48a9468500294af8730
Sstark97 <aitor@leanmind.es>
2023-08-28 19:50:37 +0100

    Test(:white_check_mark:): PasswordShould get_an_error_message_with_a_password_of_8_characters_or_less

--- src/main/java/com/sstark97/user_signup_spring/domain/model/Password.java
+++ src/main/java/com/sstark97/user_signup_spring/domain/model/Password.java
@@ -1,5 +1,18 @@
 package com.sstark97.user_signup_spring.domain.model;
 
-public class Password {
+import io.vavr.control.Either;
 
+public class Password {
+    private String password;
+
+    private Password(String password) {
+        this.password = password;
+    }
+
+    public static Either<String, Password> of(String password) {
+        if(password.length() < 8) {
+            return Either.left("The password must have 8 characters or more");
+        }
+        return Either.right(new Password(password));
+    }
 }
--- src/test/java/com/sstark97/user_signup_spring/PasswordShould.java
+++ src/test/java/com/sstark97/user_signup_spring/PasswordShould.java
@@ -1,6 +1,7 @@
 package com.sstark97.user_signup_spring;
 
 import com.sstark97.user_signup_spring.domain.model.Password;
+import io.vavr.control.Either;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -8,7 +9,7 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class PasswordShould {
     @Test
     void get_an_error_message_with_a_password_of_8_characters_or_less() {
-        Password password = Password.of("");
+        Either<String, Password> password = Password.of("");
 
         assertThat(password.isLeft()).isTrue();
         assertThat(password.getLeft()).isEqualTo("The password must have 8 characters or more");
