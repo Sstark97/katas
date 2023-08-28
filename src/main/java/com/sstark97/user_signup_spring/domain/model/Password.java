0310e76aae3981abf62e3bdc1bbaa03fff561ba6
Sstark97 <aitor@leanmind.es>
2023-08-28 19:47:57 +0100

    Test(:rotating_light:): PasswordShould get_an_error_message_with_a_password_of_8_characters_or_less

---
+++ src/main/java/com/sstark97/user_signup_spring/domain/model/Password.java
@@ -0,0 +1,5 @@
+package com.sstark97.user_signup_spring.domain.model;
+
+public class Password {
+
+}
---
+++ src/test/java/com/sstark97/user_signup_spring/PasswordShould.java
@@ -0,0 +1,16 @@
+package com.sstark97.user_signup_spring;
+
+import com.sstark97.user_signup_spring.domain.model.Password;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class PasswordShould {
+    @Test
+    void get_an_error_message_with_a_password_of_8_characters_or_less() {
+        Password password = Password.of("");
+
+        assertThat(password.isLeft()).isTrue();
+        assertThat(password.getLeft()).isEqualTo("The password must have 8 characters or more");
+    }
+}
