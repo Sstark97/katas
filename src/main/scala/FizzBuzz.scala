7121a56ee1576c4ac53d8fb535047957266ebacf
Sstark97 <aitor@leanmind.es>
2023-09-19 18:11:00 +0100

    :recycle: Extract method isFizz and isBuzz

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,6 +1,14 @@
-val fizzBuzz: Int => String = (n: Int) => {
-  val fizz = if n % 3 == 0 then "fizz" else ""
-  val buzz = if n % 5 == 0 then "buzz" else ""
+def isFizz(n: Int): Boolean = {
+  n % 3 == 0
+}
+
+def isBuzz(n: Int): Boolean = {
+  n % 5 == 0
+}
+
+def fizzBuzz(n: Int): String = {
+  val fizz = if isFizz(n) then "fizz" else ""
+  val buzz = if isBuzz(n) then "buzz" else ""
   val fizzBuzz = fizz + buzz
 
   if fizzBuzz != "" then
