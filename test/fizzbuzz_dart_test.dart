2905e6c2ed06bdd465fbaa2891a0f464f9a63741
Sstark97 <aitor@leanmind.es>
2023-10-12 21:10:06 +0100

    :recycle: Remove unnecessary tests

--- test/fizzbuzz_dart_test.dart
+++ test/fizzbuzz_dart_test.dart
@@ -3,22 +3,6 @@ import 'package:test/test.dart';
 
 void main() {
   group("fizzbuzz for", () {
-    test("the number 1 must be '1'", () {
-      expect(fizzbuzz(1), equals("1"));
-    });
-
-    test("the number 3 must be 'fizz'", () {
-      expect(fizzbuzz(3), equals("fizz"));
-    });
-
-    test("the number 5 must be 'buzz'", () {
-      expect(fizzbuzz(5), equals("buzz"));
-    });
-
-    test("the number 15 must be 'fizzbuzz'", () {
-      expect(fizzbuzz(15), equals("fizzbuzz"));
-    });
-
     test("any number divisible by 3 must be 'fizz'", () {
       expect(fizzbuzz(6), equals("fizz"));
     });
