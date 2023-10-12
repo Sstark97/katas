5df966379e9929430aada3087f4cd4460335b47a
Sstark97 <aitor@leanmind.es>
2023-10-12 20:59:55 +0100

    :recycle: Extract method to check if isFizz and if isBuzz

--- lib/fizzbuzz_dart.dart
+++ lib/fizzbuzz_dart.dart
@@ -1,10 +1,14 @@
 String fizzbuzz(int number) {
   if (number == 15) {
     return "fizzbuzz";
-  } else if(number == 3) {
+  } else if(isFizz(number)) {
     return "fizz";
-  } else if(number == 5) {
+  } else if(isBuzz(number)) {
     return "buzz";
   }
   return "1";
 }
+
+bool isBuzz(int number) => number == 5;
+
+bool isFizz(int number) => number == 3;
