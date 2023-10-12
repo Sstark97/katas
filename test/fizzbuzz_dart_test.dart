e8347b5cc872912bb66d17b6138bcf1126db7d5d
Sstark97 <aitor@leanmind.es>
2023-10-12 20:54:25 +0100

    Test(:rotating_light:): fizzbuzz for the number 5 must be 'buzz'

--- test/fizzbuzz_dart_test.dart
+++ test/fizzbuzz_dart_test.dart
@@ -2,13 +2,17 @@ import 'package:fizzbuzz_dart/fizzbuzz_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("fizzbuzz", () {
-    test("number 1 must be '1'", () {
+  group("fizzbuzz for", () {
+    test("the number 1 must be '1'", () {
       expect(fizzbuzz(1), equals("1"));
     });
 
-    test("number 3 must be 'fizz'", () {
+    test("the number 3 must be 'fizz'", () {
       expect(fizzbuzz(3), equals("fizz"));
+    });
+
+    test("the number 5 must be 'buzz'", () {
+      expect(fizzbuzz(5), equals("buzz"));
     });
   });
 }
